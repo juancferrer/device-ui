2e278a864e5e052b9e74450ec9cfeee14046e818
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2025-05-16 00:47:59 -0500

    test

--- generated/ui_320x240/screens.c
+++ generated/ui_320x240/screens.c
@@ -63,7 +63,7 @@ void create_screen_boot_screen() {
             lv_obj_set_size(obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
             lv_obj_set_style_align(obj, LV_ALIGN_BOTTOM_MID, LV_PART_MAIN | LV_STATE_DEFAULT);
             lv_obj_set_style_text_font(obj, &ui_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
-            lv_label_set_text(obj, "Kitty Mesh");
+            lv_label_set_text(obj, "Kitty Mesh2");
         }
         {
             // FirmwareLabel
